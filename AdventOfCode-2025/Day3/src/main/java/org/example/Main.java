f95a97dae19334aca9f4522084a652c3c024bce1
James Deaton <jamesdeaton72@gmail.com>
2025-12-04 09:57:22 -0500

    Update Main.java

--- AdventOfCode-2025/Day3/src/main/java/org/example/Main.java
+++ AdventOfCode-2025/Day3/src/main/java/org/example/Main.java
@@ -6,8 +6,6 @@ import java.nio.file.Path;
 import java.util.Arrays;
 import java.util.stream.Collectors;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         String[] banks;
